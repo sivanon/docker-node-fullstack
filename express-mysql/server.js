a50bb0fa8f4a50d98a0614582a2e63aede1a321d
sivanon <74519234+sivanon@users.noreply.github.com>
2023-02-22 11:52:11 +0700

    first commit

---
+++ express-mysql/server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+const mysql = require('mysql2');
+
+var host = 'localhost';
+if (process.env.NODE_ENV == 'production'){
+    host = 'mysql-server'
+}
+
+
+const connection = mysql.createConnection({
+  host: 'host',
+  user: 'root',
+  password: '1234',
+  database: 'travel'
+});
+
+
+app.get('/attractions', function (req, res, next) {
+    connection.query(
+        'SELECT * FROM attractions',
+        function(err, results, fields) {
+            res.json(results);
+        }
+    );
+})
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 3000')
+})
